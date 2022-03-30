7fead4a7992fd3231e7713a01f9b4532468d6e53
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2022-03-30 12:21:11 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,9 @@
+import numpy as np
+np.array=[[[1,2,3,4],[2,3,4,5],[4,5,6,7],[2,6,7,5]],
+[[1,2,3,4],[2,3,4,5],[4,5,6,7],[2,6,7,5]],
+[[1,2,3,4],[2,3,4,5],[4,5,6,7],[2,6,7,5]],
+[[1,2,3,4],[2,3,4,5],[4,5,6,7],[2,6,7,5]]]
+print("Display all elements excluding the first row",np.array[1,:])
+print("Display all the elements excluding the last column", np.array[:,:4])
+
+
